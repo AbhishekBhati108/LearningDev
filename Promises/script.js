e0343d7985a148f76148722d1f75f1e5ddf4c1a7
AbhishekBhati108 <abhishek10062000@gmail.com>
2023-09-20 12:20:57 +0530

    updated

--- Promises/script.js
+++ Promises/script.js
@@ -1,4 +1,4 @@
-// const cart = ["pants", "curta", "shoes"];
+const cart = ["pants", "curta", "shoes"];
 
 // createOrder(cart, function (orderID) {
 //   proceedToPayment(orderID);
@@ -10,7 +10,28 @@
 //   proceedToPayment(orderID);
 // });
 
-const GITHUB_API = "https://api.github.com/users/akshaymarch7";
+// const GITHUB_API = "https://api.github.com/users/akshaymarch7";
 
-const user = fetch(GITHUB_API);
-console.log(user);
+// const user = fetch(GITHUB_API);
+// console.log(user);
+
+// callback hell
+createOrder(cart, function (orderID) {
+  proceedToPayment(orderID, function (paymentInfo) {
+    showOrderSummery(paymentInfo, function () {
+      updateWalletBalance();
+    });
+  });
+});
+
+// Using Promise chaining we can avoid callback hell
+createOrder(cart)
+  .then(function (orderID) {
+    proceedToPayment(orderID);
+  })
+  .then(function (paymentInfo) {
+    showOrderSummery(paymentInfo);
+  })
+  .then(function () {
+    updateWalletBalance();
+  });
