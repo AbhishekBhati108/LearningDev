3f81e1a35335ec1f77020924f59a95b17c0c276b
AbhishekBhati108 <abhishek10062000@gmail.com>
2023-10-10 17:28:05 +0530

    update the files

---
+++ PracticeJS/App.js
@@ -0,0 +1,19 @@
+"use strict";
+
+//console.log(getName3());
+// console.log(getName2());
+// console.log(getName3());
+
+var getName3 = function () {
+  console.log("Namaste JavaScript4");
+};
+
+var getName2 = () => {
+  // use fat arrow function
+  console.log("Namaste JavaScript");
+};
+
+function getName() {
+  console.log("Namaste JavaScript");
+}
+console.log(getName3());
